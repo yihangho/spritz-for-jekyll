70e7da4e8cecc525c6e2eed88f9941d223f108eb
Yihang Ho <hoyihang5@gmail.com>
2014-04-22 22:01:42 +0800

    login_success.html does not have to be manually added now.

--- lib/spritz_for_jekyll.rb
+++ lib/spritz_for_jekyll.rb
@@ -14,7 +14,7 @@ module Spritz
   window.jQuery || document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">\\x3C/script>');
   var SpritzSettings = {
     clientId: "#{client_id}",
-    redirectUri: "#{base_url}/login_success.html"
+    redirectUri: "data:text/html;base64,PCFkb2N0eXBlIGh0bWw+PGh0bWw+PGhlYWQ+PG1ldGEgY2hhcnNldD0idXRmLTgiIC8+PG1ldGEgbmFtZT0idmlld3BvcnQiIGNvbnRlbnQ9IndpZHRoPWRldmljZS13aWR0aCwgaW5pdGlhbC1zY2FsZT0xIiAvPjx0aXRsZT5TcHJpdHogTG9naW4gU3VjY2VzczwvdGl0bGU+PC9oZWFkPjxib2R5PjxzY3JpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0Ij52YXIgaGFzaD13aW5kb3cubG9jYXRpb24uaGFzaCxvcmlnaW49d2luZG93LmxvY2F0aW9uLnByb3RvY29sKyIvLyIrd2luZG93LmxvY2F0aW9uLmhvc3Q7aWYodHlwZW9mKGxvY2FsU3RvcmFnZSkhPT0idW5kZWZpbmVkIil7dHJ5e2xvY2FsU3RvcmFnZS5zZXRJdGVtKCJzcHJpdHouYXV0aFJlc3BvbnNlIixoYXNoKX1jYXRjaChlKXtpZihjb25zb2xlKXtjb25zb2xlLmxvZyhlLCJDYW4ndCB3cml0ZSB0byBsb2NhbFN0b3JhZ2UiKX19fWlmKHdpbmRvdy5vcGVuZXIpe3dpbmRvdy5vcGVuZXIucG9zdE1lc3NhZ2UoaGFzaCxvcmlnaW4pfTs8L3NjcmlwdD48L2JvZHk+PC9odG1sPg=="
   };
 </script>
 <script id="spritzjs" type="text/javascript" src="//sdk.spritzinc.com/js/1.0/js/spritz.min.js"></script>
@@ -43,7 +43,7 @@ HEREDOC
   window.jQuery || document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">\\x3C/script>');
   var SpritzSettings = {
     clientId: "#{client_id}",
-    redirectUri: "#{base_url}/login_success.html"
+    redirectUri: "data:text/html;base64,PCFkb2N0eXBlIGh0bWw+PGh0bWw+PGhlYWQ+PG1ldGEgY2hhcnNldD0idXRmLTgiIC8+PG1ldGEgbmFtZT0idmlld3BvcnQiIGNvbnRlbnQ9IndpZHRoPWRldmljZS13aWR0aCwgaW5pdGlhbC1zY2FsZT0xIiAvPjx0aXRsZT5TcHJpdHogTG9naW4gU3VjY2VzczwvdGl0bGU+PC9oZWFkPjxib2R5PjxzY3JpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0Ij52YXIgaGFzaD13aW5kb3cubG9jYXRpb24uaGFzaCxvcmlnaW49d2luZG93LmxvY2F0aW9uLnByb3RvY29sKyIvLyIrd2luZG93LmxvY2F0aW9uLmhvc3Q7aWYodHlwZW9mKGxvY2FsU3RvcmFnZSkhPT0idW5kZWZpbmVkIil7dHJ5e2xvY2FsU3RvcmFnZS5zZXRJdGVtKCJzcHJpdHouYXV0aFJlc3BvbnNlIixoYXNoKX1jYXRjaChlKXtpZihjb25zb2xlKXtjb25zb2xlLmxvZyhlLCJDYW4ndCB3cml0ZSB0byBsb2NhbFN0b3JhZ2UiKX19fWlmKHdpbmRvdy5vcGVuZXIpe3dpbmRvdy5vcGVuZXIucG9zdE1lc3NhZ2UoaGFzaCxvcmlnaW4pfTs8L3NjcmlwdD48L2JvZHk+PC9odG1sPg=="
   };
 </script>
 <script id="spritzjs" type="text/javascript" src="//sdk.spritzinc.com/js/1.0/js/spritz.min.js"></script>
