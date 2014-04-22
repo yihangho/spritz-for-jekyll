9ed16d880e9df5b53e9f277ce1491a1b661989ce
Yihang Ho <hoyihang5@gmail.com>
2014-04-22 20:19:59 +0800

    Support spritz_scripts liquid tag.

--- lib/spritz_for_jekyll.rb
+++ lib/spritz_for_jekyll.rb
@@ -25,4 +25,41 @@ HEREDOC
       end
     end
   end
+
+  class SpritzScriptTag < Liquid::Tag
+    def render(context)
+      site = get_site(context.environments)
+      return if site.nil?
+
+      client_id = site["spritz_client_id"]
+      base_url  = site["url"]
+
+      return if client_id.nil?
+      puts "url should be defined in order for Spritz to work." if base_url.nil?
+
+      snippet = <<HEREDOC
+<script>
+  window.jQuery || document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">\\x3C/script>');
+  var SpritzSettings = {
+    clientId: "#{client_id}",
+    redirectUri: "#{base_url}/login_success.html"
+  };
+</script>
+<script id="spritzjs" type="text/javascript" src="//sdk.spritzinc.com/js/1.0/js/spritz.min.js"></script>
+<p><div data-role="spritzer"></div></p>
+HEREDOC
+    end
+
+    private
+
+    def get_site(environments)
+      environments.each do |e|
+        if e.has_key?("site")
+          return e["site"]
+        end
+      end
+    end
+  end
 end
+
+Liquid::Template.register_tag('spritz_scripts', Spritz::SpritzScriptTag)
